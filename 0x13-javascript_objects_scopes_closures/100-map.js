d737695ca845cc552a403a551a7761644914c492
Demilade362 <ademolademilade362@gmail.com>
2023-11-16 11:05:54 +0100

    python objects

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 
+console.log(require("./100-data.js"));
+
 const newList = list.map((val, idx) => val * idx);
 console.log(list);
 console.log(newList);
